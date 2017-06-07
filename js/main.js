97ebac460d3ec177b1563abc4adcbc6ebd55613e
Quiddale <qosullivan962@gmail.com>
2017-06-07 13:19:52 +0100

    message

---
+++ js/main.js
@@ -0,0 +1,62 @@
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	},
+];
+
+var cardInPlay = [];
+
+var checkForMatch = function(){
+	    if (cardInPlay.length === 2) {
+		if (cardInPlay[0] === cardInPlay[1]){
+			alert("You found a match");
+		}else{
+			alert("Sorry, try again.");
+		}
+	}
+};
+
+var flipCard = function() {
+	cardId = this.getAttribute('data-id');
+	cardInPlay.push(cards[cardId].rank);
+	console.log("User flipped" + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	this.setAttribute('src',cards[cardId].cardImage);
+
+	checkForMatch();
+};
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++){
+var cardElement = document.createElement('img');
+cardElement.setAttribute('src', "images/back.png");
+cardElement.setAttribute('data-id', i);
+document.getElementById('game-board').appendChild(cardElement);
+
+cardElement.addEventListener('click', flipCard);
+}
+};
+
+createBoard();
+	
